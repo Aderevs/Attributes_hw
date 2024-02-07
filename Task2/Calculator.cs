5beb89d811508fb9435dc8758aa3033e0ea3c642
Yura <aderevs@gmail.com>
2024-02-07 16:39:13 +0200

    working version of task 2

---
+++ Task2/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal static class Calculator
+    {
+        [Obsolete("this mathod is obsolete use new method Add instead")]
+        public static int Addition(int a, int b)
+        {
+            int sum;
+            sum = a + b;
+            return sum;
+        }
+
+        [Obsolete("this mathod is obsolete use new method Sub instead")]
+        public static int Substruction(int a, int b)
+        {
+            int difference;
+            difference = a - b;
+            return difference;
+        }
+
+        [Obsolete("this mathod is obsolete use new method Mul instead")]
+        public static int Multiplying(int a, int b)
+        {
+            int product = 0;
+            for (int i = 0; i < b; i++)
+            {
+                product += a;
+            }
+            return product;
+        }
+
+        [Obsolete("this mathod is obsolete use new method Div instead", true)]
+        //only returning values from 0 to 10
+        public static int Division(int a, int b)
+        {
+            if (a == 0)
+            {
+                return 0;
+            }
+            int product = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                product = Multiplying(b, i);
+                if (product == a)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enrter X:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enrter Y:");
+            int y = int.Parse(Console.ReadLine());
+            /*Console.WriteLine($"X + Y = {Calculator.Addition(x,y)}");
+            Console.WriteLine($"X - Y = {Calculator.Substruction(x,y)}");
+            Console.WriteLine($"X * Y = {Calculator.Multiplying(x,y)}");
+            Console.WriteLine($"X / Y = {Calculator.Division(x,y)}");*/
+            Console.WriteLine($"X + Y = {Calculator.Add(x, y)}");
+            Console.WriteLine($"X - Y = {Calculator.Sub(x, y)}");
+            Console.WriteLine($"X * Y = {Calculator.Mul(x, y)}");
+            Console.WriteLine($"X / Y = {Calculator.Div(x, y)}");
+
         }
     }
 }
